b042efd99e3bef8683be7b513f27337e630ba686
dlee08 <dlee08.nyc@gmail.com>
2026-03-31 01:27:15 -0400

    added barebones local server

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,14 @@
-import pandas as pd
-import sqlite
-data = pd.read_csv("IKEA_product_catalog.csv")
+# import pandas as pd
+# import sqlite
+# data = pd.read_csv("IKEA_product_catalog.csv")
 
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+  return "the big leagues are calling us twin"
+
+if __name__ == "__main__":
+  app.debug = True
+  app.run()
